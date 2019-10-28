6b2a4fb26b13dd942fddb732d929d5608de7a935
Louis Hella <l.hella@afelio.be>
2019-10-28 09:29:08 +0100

    first commit

---
+++ src/main/java/com/lewis/testLint/TestLintApplication.java
@@ -0,0 +1,13 @@
+package com.lewis.testLint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestLintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lewis/testLint/TestLintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lewis.testLint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestLintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
